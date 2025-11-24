2c6fd0891e1959a0df886f6bdcfb6fafd2106302
unknown <yahya@parkonic.com>
2025-11-24 11:04:11 +0400

    hiding env

---
+++ config/db_config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // 'connection': {
+  //   'host': 'localhost',
+  //   'user': 'OnstreetAdmin',
+  //   'password': "Onstreet123@4%<>.,8_!#@$"
+  // },
+  'connection': {
+    'host': 'localhost',
+    'user': 'root',
+    'password': ""
+  },
+  // 'database': 'onstreet',
+  'database': 'OnStreetOSPCloud5',
+  'users_table': 'users',
+  'locations_table': 'locations',
+  'zones_table': 'zones',
+  'poles_table': 'poles',
+  'cameras_table': 'cameras',
+  'log_table': 'logs',
+  'notification_table': 'notifications',
+  'permission_table': 'permissions',
+  'user_permission': 'user_permissions',
+};
